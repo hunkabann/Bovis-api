c41fbb250e82a3a9a53bf6f36a6764eb3f93d896
elbaltezar <norberto.baltezar@gmail.com>
2023-06-22 10:27:36 -0600

    update

--- Bovis.Common.Model/Tables/TB_Cat_EdoCivil.cs
+++ Bovis.Common.Model/Tables/TB_Cat_EdoCivil.cs
@@ -5,8 +5,8 @@ namespace Bovis.Common.Model.Tables
 	[Table(Schema = "dbo", Name = "tb_cat_edo_civil")]
 	public class TB_Cat_EdoCivil
 	{
-		[Column("NukidedoCivil"), NotNull, PrimaryKey, Identity] public int IdEdoCivil { get; set; }
-		[Column("ChedoCivil"), NotNull] public string EdoCivil { get; set; }
+		[Column("Nukidedo_civil"), NotNull, PrimaryKey, Identity] public int IdEdoCivil { get; set; }
+		[Column("Chedo_civil"), NotNull] public string EdoCivil { get; set; }
 		[Column("Boactivo"), NotNull] public bool Activo { get; set; }
 	}
 }
--- Bovis.Common.Model/Tables/TB_Cat_TipoSangre.cs
+++ Bovis.Common.Model/Tables/TB_Cat_TipoSangre.cs
@@ -5,8 +5,8 @@ namespace Bovis.Common.Model.Tables
 	[Table(Schema = "dbo", Name = "tb_cat_tipo_sangre")]
 	public class TB_Cat_TipoSangre
 	{
-		[Column("NukidtipoSangre"), NotNull, PrimaryKey, Identity] public int IdTipoSangre { get; set; }
-		[Column("ChtipoSangre"), NotNull] public string TipoSangre { get; set; }
+		[Column("Nukidtipo_sangre"), NotNull, PrimaryKey, Identity] public int IdTipoSangre { get; set; }
+		[Column("Chtipo_sangre"), NotNull] public string TipoSangre { get; set; }
 		[Column("Boactivo"), NotNull] public bool Activo { get; set; }
 	}
 }
