a44330b164fb1806c4e3fe3caae7da95900f19c6
elbaltezar <norberto.baltezar@gmail.com>
2023-06-22 21:15:33 -0600

    update apis

--- Bovis.Common.Model/Tables/TB_Cat_TipoContrato.cs
+++ Bovis.Common.Model/Tables/TB_Cat_TipoContrato.cs
@@ -7,7 +7,7 @@ namespace Bovis.Common.Model.Tables
 	{
 		[Column("Nukidtipo_contrato"), NotNull, PrimaryKey, Identity] public int IdTipoContrato { get; set; }
 		[Column("Chcontrato"), NotNull] public string Contrato { get; set; }
+		[Column("Chve_contrato"), Nullable] public byte? VeContrato { get; set; }
 		[Column("Boactivo"), NotNull] public bool Activo { get; set; }
-		[Column("Chcve_contrato"), Nullable] public byte? Cve { get; set; }
 	}
 }
