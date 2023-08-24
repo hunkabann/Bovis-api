3edea0de8ba710e69f13645fa17e0e85f4027802
elbaltezar <norberto.baltezar@gmail.com>
2023-08-23 20:04:00 -0600

    Update

--- Bovis.Data/DorData.cs
+++ Bovis.Data/DorData.cs
@@ -618,28 +618,20 @@ namespace Bovis.Data
         {
             (bool Success, string Message) resp = (true, string.Empty);
 
-            try
-            {
-                int id = Convert.ToInt32(registro["id"].ToString());
-                int id_empleado = Convert.ToInt32(registro["id_empleado"].ToString());
-                decimal real = Convert.ToDecimal(registro["real"].ToString());
+            int id = Convert.ToInt32(registro["id"].ToString());
+            int id_empleado = Convert.ToInt32(registro["id_empleado"].ToString());
+            decimal real = Convert.ToDecimal(registro["real"].ToString());
 
-                using (var db = new ConnectionDB(dbConfig))
-                {
-                    var res_update_real = await db.tB_Dor_Meta_Proyectos.Where(x => x.Empleado == id_empleado)
-                        .UpdateAsync(x => new TB_Dor_Meta_Proyecto
-                        {
-                            Real = real
-                        }) > 0;
-
-                    resp.Success = res_update_real;
-                    resp.Message = res_update_real == default ? "Ocurrio un error al actualizar registro." : string.Empty;
-                }
-            }
-            catch (Exception ex)
+            using (var db = new ConnectionDB(dbConfig))
             {
-                resp.Success = true;
-                resp.Message = string.Format("[ERROR]: {0} - [Request Body]: {1}", ex.Message, registro.ToString());
+                var res_update_real = await db.tB_Dor_Meta_Proyectos.Where(x => x.Empleado == id_empleado)
+                    .UpdateAsync(x => new TB_Dor_Meta_Proyecto
+                    {
+                        Real = real
+                    }) > 0;
+
+                resp.Success = res_update_real;
+                resp.Message = res_update_real == default ? "Ocurrio un error al actualizar registro." : string.Empty;
             }
             return resp;
         }
