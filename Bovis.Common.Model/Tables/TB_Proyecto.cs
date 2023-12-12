38a057707c23db73bee58a87102a027c255931f1
elbaltezar <norberto.baltezar@gmail.com>
2023-12-12 16:14:36 -0600

    Update PCS Service

--- Bovis.Common.Model/Tables/TB_Proyecto.cs
+++ Bovis.Common.Model/Tables/TB_Proyecto.cs
@@ -9,20 +9,20 @@ namespace Bovis.Common.Model.Tables
 		[Column("Chproyecto"), NotNull] public string Proyecto { get; set; }
 		[Column("Chalcance"), NotNull] public string Alcance { get; set; }
 		[Column("Chcp"), Nullable] public string? Cp { get; set; }
-		[Column("Chciudad"), NotNull] public string Ciudad { get; set; }
-		[Column("Nukidpais"), NotNull] public int IdPais { get; set; }
-		[Column("Nukidestatus"), NotNull] public int IdEstatus { get; set; }
-		[Column("Nukidsector"), NotNull] public int IdSector { get; set; }
-		[Column("nukidtipo_proyecto"), Nullable] public int IdTipoProyecto { get; set; }
+		[Column("Chciudad"), Nullable] public string? Ciudad { get; set; }
+		[Column("Nukidpais"), Nullable] public int? IdPais { get; set; }
+		[Column("Nukidestatus"), Nullable] public int? IdEstatus { get; set; }
+		[Column("Nukidsector"), Nullable] public int? IdSector { get; set; }
+		[Column("nukidtipo_proyecto"), Nullable] public int? IdTipoProyecto { get; set; }
 		[Column("nukidresponsable_preconstruccion"), Nullable] public int? IdResponsablePreconstruccion { get; set; }
 		[Column("nukidresponsable_construccion"), Nullable] public int? IdResponsableConstruccion { get; set; }
 		[Column("nukidresponsable_ehs"), Nullable] public int? IdResponsableEhs { get; set; }
 		[Column("nukidresponsable_supervisor"), Nullable] public int? IdResponsableSupervisor { get; set; }
-		[Column("Nukidcliente"), NotNull] public int IdCliente { get; set; }
-		[Column("Nukidempresa"), NotNull] public int IdEmpresa { get; set; }
-		[Column("nukiddirector_ejecutivo"), NotNull] public int IdDirectorEjecutivo { get; set; }
-		[Column("nucosto_promedio_m2"), NotNull] public decimal CostoPromedioM2 { get; set; }
-		[Column("dtfecha_ini"), NotNull] public DateTime FechaIni { get; set; }
+		[Column("Nukidcliente"), Nullable] public int? IdCliente { get; set; }
+		[Column("Nukidempresa"), Nullable] public int? IdEmpresa { get; set; }
+		[Column("nukiddirector_ejecutivo"), Nullable] public int? IdDirectorEjecutivo { get; set; }
+		[Column("nucosto_promedio_m2"), Nullable] public decimal? CostoPromedioM2 { get; set; }
+		[Column("dtfecha_ini"), Nullable] public DateTime FechaIni { get; set; }
 		[Column("dtfecha_fin"), Nullable] public DateTime? FechaFin { get; set; }
 	}
 }
