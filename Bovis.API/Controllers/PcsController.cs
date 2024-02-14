93343d15e816eaf472ffd9ecdb06e5b3487d9f21
elbaltezar <norberto.baltezar@gmail.com>
2024-02-14 09:57:40 -0600

    Update

--- Bovis.API/Controllers/PcsController.cs
+++ Bovis.API/Controllers/PcsController.cs
@@ -117,8 +117,20 @@ namespace Bovis.API.Controllers
         [HttpPut, Route("Proyectos/FechaAuditoria")]
         public async Task<IActionResult> UpdateProyectoFechaAuditoria([FromBody] JsonObject registro)
         {
-            var query = await _pcsQueryService.UpdateProyectoFechaAuditoria(registro);
-            return Ok(query);
+            IHeaderDictionary headers = HttpContext.Request.Headers;
+            string email = headers["email"];
+            string nombre = headers["nombre"];
+            JsonObject registroJsonObject = new JsonObject();
+            registroJsonObject.Add("Registro", registro);
+            registroJsonObject.Add("Nombre", nombre);
+            registroJsonObject.Add("Usuario", email);
+            registroJsonObject.Add("Roles", string.Empty);
+            registroJsonObject.Add("TransactionId", TransactionId);
+            registroJsonObject.Add("Rel", 1053);
+
+            var query = await _pcsQueryService.UpdateProyectoFechaAuditoria(registroJsonObject);
+            if (query.Message == string.Empty) return Ok(query);
+            else return BadRequest(query.Message);
         }
         #endregion Proyectos
 
