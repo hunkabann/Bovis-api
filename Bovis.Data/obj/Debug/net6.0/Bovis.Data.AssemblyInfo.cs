9e0d2831c406ab697fc1981bec8c6d65ffdb84ca
elbaltezar <norberto.baltezar@gmail.com>
2024-03-01 18:24:02 -0600

    Update Auditoría, improve performance to responde the documents of auditoria

--- Bovis.Data/obj/Debug/net6.0/Bovis.Data.AssemblyInfo.cs
+++ Bovis.Data/obj/Debug/net6.0/Bovis.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bovis.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89143b499318a3435ded2e39eb40ade1554ab35a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86fb5053116f228c893a5a346180c628a1b3366b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bovis.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bovis.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
