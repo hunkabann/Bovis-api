869fbcf3e9b65b58f123f04f935224a65528ad9f
elbaltezar <norberto.baltezar@gmail.com>
2024-03-05 23:02:14 -0600

    Update - Factura Service

--- Bovis.Data.Interface/obj/Debug/net6.0/Bovis.Data.Interface.AssemblyInfo.cs
+++ Bovis.Data.Interface/obj/Debug/net6.0/Bovis.Data.Interface.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bovis.Data.Interface")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b9c387026dc6daec468eaa14c59733ae7c8c72c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bde1a04f5c151795e614061be1822b7f30808ece")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bovis.Data.Interface")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bovis.Data.Interface")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
