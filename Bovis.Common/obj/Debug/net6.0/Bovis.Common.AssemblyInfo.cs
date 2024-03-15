30da02e11b93cd520934d2a2bb12339687ff2812
elbaltezar <norberto.baltezar@gmail.com>
2024-03-14 19:50:15 -0600

    Update compilation packages

--- Bovis.Common/obj/Debug/net6.0/Bovis.Common.AssemblyInfo.cs
+++ Bovis.Common/obj/Debug/net6.0/Bovis.Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bovis.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14cbd205934a88558ba6bd6ff4c048acca4d4d5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d75ff1a377534367693570b34290e4fc66b3b78f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bovis.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bovis.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
