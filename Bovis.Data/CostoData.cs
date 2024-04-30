20f1ce5e194971c091331ccb889a2008cf845954
elbaltezar <norberto.baltezar@gmail.com>
2024-04-30 14:38:29 -0600

    Update

--- Bovis.Data/CostoData.cs
+++ Bovis.Data/CostoData.cs
@@ -313,7 +313,8 @@ namespace Bovis.Data
                         registro.NuMes = DateTime.Now.Month;
                         registro.NuAnno = DateTime.Now.Year;
                         registro.FechaActualizacion = DateTime.Now;
-                        if (registro?.AvgBonoAnualEstimado == 0)
+                        if (registro?.AvgBonoAnualEstimado != 0 && registro?.SgmmCostoTotalAnual != 0 &&
+                            registro?.SvCostoTotalAnual != 0 && registro?.VaidCostoMensual != 0)
                         {
                             registro.AvgBonoAnualEstimado = avg_bono_anual_estimado;
                             registro.SgmmCostoTotalAnual = sgmm_costo_total_anual;
