64616caa7d79754c82c1178172fd73142c636a79
hunkabann <36930991+hunkabann@users.noreply.github.com>
2024-07-13 02:07:33 +0200

    modifica dedicacin de otros float en ugar de int

--- Bovis.API/obj/Debug/net6.0/Bovis.API.AssemblyInfo.cs
+++ Bovis.API/obj/Debug/net6.0/Bovis.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bovis.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dfad7cf3fcd48345a02763f742573756d255d96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0275818f4a000120b7455dbece941346929a6f71")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bovis.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bovis.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
