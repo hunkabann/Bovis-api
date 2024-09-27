ace1fcb7acf1c9eabbd6eb927d8b0e405948e134
hunkabann <36930991+hunkabann@users.noreply.github.com>
2024-09-27 03:37:30 +0200

    agregar condicion de activo por la tabla timesheet proyecto

--- Bovis.Data/obj/Debug/net6.0/Bovis.Data.AssemblyInfo.cs
+++ Bovis.Data/obj/Debug/net6.0/Bovis.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bovis.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58217a6283928eff190b5cc45edc0eb0d6228d3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05cddded9ab0ccdf2faaa6839dd0e84a1c5b093a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bovis.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bovis.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
