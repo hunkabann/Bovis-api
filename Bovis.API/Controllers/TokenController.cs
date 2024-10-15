048d061d558b062645814b3282a232b5a75b3432
hunkabann <36930991+hunkabann@users.noreply.github.com>
2024-10-15 14:45:53 -0600

    Update TokenController.cs

--- Bovis.API/Controllers/TokenController.cs
+++ Bovis.API/Controllers/TokenController.cs
@@ -68,7 +68,7 @@ namespace Bovis.API.Controllers
                 var token = new SecurityTokenDescriptor
                 {
                     Subject = claims,                    
-                    Expires = DateTimeOffset.UtcNow.AddHours(0.25).UtcDateTime,
+                    Expires = DateTimeOffset.UtcNow.AddHours(1).UtcDateTime,
                     SigningCredentials = creds
                 };
 
