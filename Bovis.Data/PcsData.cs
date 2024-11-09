9740e3dcba98d253e8a64b133cef05ddece027ba
hunkabann <36930991+hunkabann@users.noreply.github.com>
2024-11-08 21:05:14 -0600

    Update PcsData.cs where p.IdEstatus != 3

--- Bovis.Data/PcsData.cs
+++ Bovis.Data/PcsData.cs
@@ -97,7 +97,7 @@ namespace Bovis.Data
                 using (var db = new ConnectionDB(dbConfig))
             {
                 var proyectos = await (from p in db.tB_Proyectos
-                                       where p.IdEstatus == 1
+                                       where p.IdEstatus != 3
                                        select p).ToListAsync();
                 return proyectos;
             }
