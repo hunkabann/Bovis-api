dbd2df8538ad87be33179c0a50032fe269756dac
hunkabann <jmmorales@hunkabann.com.mx>
2025-01-02 20:07:37 -0600

    actulizacion

--- Bovis.API/obj/Debug/net6.0/Bovis.API.AssemblyInfo.cs
+++ Bovis.API/obj/Debug/net6.0/Bovis.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bovis.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f455003a6c0941da3a0c5164a45d93669489b1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e70334dbd35be82bd6b26fe945d3da420d1347e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bovis.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bovis.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Bovis.Common/obj/Debug/net6.0/Bovis.Common.AssemblyInfo.cs
+++ Bovis.Common/obj/Debug/net6.0/Bovis.Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bovis.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34f37343acb4f862344a73fe8ecf07cbd4ef6076")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e70334dbd35be82bd6b26fe945d3da420d1347e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bovis.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bovis.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
