a029a28ca391f523a9d1fd21df0f6622b2d43125
tacear <103531682+tacear@users.noreply.github.com>
2025-04-14 13:51:54 -0600

    Update TB_ProyectoFaseEmpleado.cs

--- Bovis.Common.Model/Tables/TB_ProyectoFaseEmpleado.cs
+++ Bovis.Common.Model/Tables/TB_ProyectoFaseEmpleado.cs
@@ -5,7 +5,7 @@ namespace Bovis.Common.Model.Tables
     [Table(Schema = "dbo", Name = "tb_proyecto_fase_empleado")]
     public class TB_ProyectoFaseEmpleado
     {
-        [Column("Nukid"), NotNull, PrimaryKey, Identity] public int Id { get; set; }
+                [Column("Nukid"), NotNull, PrimaryKey, Identity] public int Id { get; set; }
         [Column("Nukidfase"), NotNull] public int IdFase { get; set; }
         [Column("Nunum_empleado_rr_hh"), NotNull] public string NumEmpleado { get; set; }
         [Column("Numes"), NotNull] public int Mes { get; set; }
@@ -13,6 +13,9 @@ namespace Bovis.Common.Model.Tables
         [Column("Nuporcentaje"), NotNull] public int Porcentaje { get; set; }
         [Column("Nucantidad"), Nullable] public decimal? Cantidad { get; set; }
         [Column("Boaplica_todos_meses"), Nullable] public bool? AplicaTodosMeses { get; set; }
+        [Column("nucosto_ini"), Nullable] public decimal? nucosto_ini { get; set; }
         [Column("Nufee"), Nullable] public decimal? Fee { get; set; }
+        [Column("Chalias"), Nullable] public string chalias { get; set; }
+        [Column("Boreembolsable"), Nullable] public bool? boreembolsable { get; set; }
     }
 }
