4b9371d0e73ec5af79c3ea5e2816f941820b9d05
Leonor Eleno <akuaryuz2@gmail.com>
2026-02-01 03:05:35 -0600

    General. Se quita obj de Bovis.Common, Bovis.Common.Model, Bovis.Common.Logging, Bovis.Common.Mapper

--- Bovis.Common.Model/obj/Debug/net6.0/Bovis.Common.Model.AssemblyInfo.cs
+++ Bovis.Common.Model/obj/Debug/net6.0/Bovis.Common.Model.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bovis.Common.Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb7e746fcb502302e358bd1dc66e7481a40d535f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bovis.Common.Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bovis.Common.Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
